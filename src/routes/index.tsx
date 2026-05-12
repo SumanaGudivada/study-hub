ae6ce1444fc2e95cd017259b0ecaeb205ef00e89
SumanaGudivada <gudivadasumana@gmail.com>
2026-05-12 20:23:33 +0530

    fixed homepage route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,5 +2,9 @@ import { createFileRoute } from "@tanstack/react-router";
 import LibraryApp from "../components/LibraryApp";
 
 export const Route = createFileRoute("/")({
-  component: LibraryApp,
+  component: Home,
 });
+
+function Home() {
+  return <LibraryApp />;
+}
